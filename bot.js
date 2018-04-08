ab47089172c0383101545d70bae792e285c3eaa6
OfficialSMuRK <38177504+OfficialSMuRK@users.noreply.github.com>
2018-04-08 12:02:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,8 +16,14 @@ client.on('guildDelete', guild => {
 
 });
 
-client.on(`guildCreate`, guild => {
+client.on('guildCreate', guild => {
     guild.defaultChannel.sendMessage(`I have joined ${guild.name}`);
+
+});
+
+client.on('guildMemberAdd', member => {
+    let guild = member.guild;
+    guild.defaultChannel.sendMessage(`Welcome ${member.user.username} to ${guild.name}`);
 
 });
       
