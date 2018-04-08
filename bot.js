aa3b3c4a5cce5ed317cf1183c3fc4adb745be372
OfficialSMuRK <38177504+OfficialSMuRK@users.noreply.github.com>
2018-04-08 14:52:05 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const ddiff = require('return-deep-diff');
 client.on('ready', () => {
   console.log('I\'m Online\nI\'m Online');
 });
