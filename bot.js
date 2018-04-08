8616117bbfeab32e2974dac7b333f21157c832df
OfficialSMuRK <38177504+OfficialSMuRK@users.noreply.github.com>
2018-04-08 14:30:10 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('guildCreate', guild => {
 
 client.on('guildMemberAdd', member => {
     let guild = member.guild;
-    guild.defaultChannel.sendMessage(`Welcome ${member.user} to the Nitrox Hub Discord Server!`)
+    guild.defaultChannel.message.send(`Welcome ${member.user} to the Nitrox Hub Discord Server!`)
 
 });
       
